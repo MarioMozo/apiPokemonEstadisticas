b35d4da35cd76145c98767ae87c2e9840ed39b86
MarioMozo <mario.psy@live.com>
2023-12-16 17:03:38 -0600

    Solucion de todos los errores, se realizaron pruebas con postman. ok

--- src/main/java/com/pokeapi/repository/BattleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pokeapi.repository;
-
-import com.pokeapi.entity.BattleEntity;
-import org.springframework.data.repository.ListCrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BattleRepository extends ListCrudRepository<BattleEntity, Integer> {
-}
--- src/main/java/com/pokeapi/service/impl/BattlePlayerServiceImpl.java
+++ src/main/java/com/pokeapi/service/impl/BattlePlayerServiceImpl.java
@@ -5,10 +5,12 @@ import com.pokeapi.persistence.IBattlePlayerDAO;
 import com.pokeapi.persistence.IBattlePokemonDAO;
 import com.pokeapi.service.IBattlePlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class BattlePlayerServiceImpl implements IBattlePlayerService {
 
     @Autowired
