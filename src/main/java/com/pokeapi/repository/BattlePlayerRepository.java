c988833cae594c39759f0d6e7ca9a3870f68669b
jose martin <josemar8713@gmail.com>
2023-12-17 18:37:46 -0600

    modificacion a solo tablas que se van usar oponent,pokemonuser,user

--- src/main/java/com/pokeapi/repository/BattlePlayerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pokeapi.repository;
-
-import com.pokeapi.entity.BattlePlayerEntity;
-import org.springframework.data.repository.ListCrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BattlePlayerRepository extends ListCrudRepository<BattlePlayerEntity,Integer> {
-}
--- src/main/java/com/pokeapi/repository/PokemonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pokeapi.repository;
-
-import com.pokeapi.entity.PokemonEntity;
-import org.springframework.data.repository.ListCrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PokemonRepository extends ListCrudRepository <PokemonEntity, Integer>{
-}
